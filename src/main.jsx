049465540387fd458d3cecc01472512116fa1320
Ephrahim Anyanje <atwoli2345@gmail.com>
2025-12-07 10:13:56 +0300

    finalization

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,17 @@
+import "./styles/global.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+
+app.add_middleware(
+  CORSMiddleware,
+  (allow_origins = ["*"]),
+  (allow_credentials = True),
+  (allow_methods = ["*"]),
+  (allow_headers = ["*"])
+);
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
