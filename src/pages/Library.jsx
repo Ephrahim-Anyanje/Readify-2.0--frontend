795f481685b36a4da05b6e00d993563744cd5acb
Ephrahim Anyanje <atwoli2345@gmail.com>
2025-12-07 10:16:25 +0300

    added library.jsx

---
+++ src/pages/Library.jsx
@@ -0,0 +1,8 @@
+export default function Library() {
+  return (
+    <div className="container">
+      <h1 className="title">Library</h1>
+      <p>Your books will show here.</p>
+    </div>
+  );
+}
